a1ab4cca0c4bd32160e20b4286540457182b4ef6
Petr Tichy <peter@ihatetomatoes.net>
2020-09-18 10:57:41 +1000

    css once transition

--- barba-css/src/js/app.js
+++ barba-css/src/js/app.js
@@ -1,1 +1,24 @@
+import barba from '@barba/core';
+import barbaCss from '@barba/css';
 
+// tell Barba to use the css plugin
+barba.use(barbaCss);
+
+// init Barba
+barba.init({
+    transitions: [
+        {
+            name: 'home',
+            beforeOnce(){
+                console.log('beforeOnce')
+            },
+            once(){
+                // with css plugin, this will not run
+                console.log('once')
+            },
+            afterOnce(){
+                console.log('afterOnce')
+            }
+        }
+    ]
+});
