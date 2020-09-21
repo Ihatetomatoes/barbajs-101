9c805347a74803d67ae738824e210ad8760f0080
Petr Tichy <peter@ihatetomatoes.net>
2020-09-21 17:45:56 +1000

    updated animationEnter

--- barba-gsap/src/js/animations/animationEnter.js
+++ barba-gsap/src/js/animations/animationEnter.js
@@ -1,7 +1,25 @@
 import gsap from 'gsap';
 
 const animationEnter = (container) => {
-    return gsap.from(container, { autoAlpha: 0, duration: 0.7, clearProps: 'all', ease: 'none'});
+
+    const activeLink = container.querySelector('a.is-active span');
+    const projects = container.querySelectorAll('.project');
+    const images = container.querySelectorAll('.image');
+    const img = container.querySelectorAll('img');
+
+    const tl = gsap.timeline({
+        defaults: {
+            duration: 0.9, ease: 'power4.out'
+        }
+    });
+
+    tl
+        .set(projects, {autoAlpha: 1})
+        .fromTo(activeLink, {xPercent: -101}, {xPercent: 0, transformOrigin: 'left'}, 0)
+        .from(images, {xPercent: -101, stagger: 0.1}, 0)
+        .from(img, {xPercent: 101, stagger: 0.1}, 0);
+
+    return tl;
 }
 
 export default animationEnter;
--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,11 +1,24 @@
 import barba from '@barba/core';
+import gsap from 'gsap';
 import { animationEnter, animationLeave } from './animations';
+
+const resetActiveLink = () => gsap.set('a.is-active span', {
+    xPercent: -100,
+    transformOrigin: 'left'
+});
 
 barba.init({
     transitions: [
         {
             once({next}){
-                animationEnter(next.container);
+                resetActiveLink();
+                gsap.from('header a', {
+                    duration: 0.6,
+                    yPercent: 100,
+                    stagger: 0.2,
+                    ease: 'power1.out',
+                    onComplete: () => animationEnter(next.container)
+                });
             },
             leave: ({current}) => animationLeave(current.container),
             enter({next}){
