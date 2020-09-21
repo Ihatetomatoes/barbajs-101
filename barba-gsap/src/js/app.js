948c03cadab70a4ddcaaa2c54275f5c5bed58841
Petr Tichy <peter@ihatetomatoes.net>
2020-09-21 13:39:55 +1000

    gsap simple tween

--- barba-gsap/src/js/app.js
+++ barba-gsap/src/js/app.js
@@ -1,3 +1,24 @@
 import barba from '@barba/core';
 import gsap from 'gsap';
 
+const animationEnter = (container) => {
+    return gsap.from(container, { autoAlpha: 0, duration: 0.7, clearProps: 'all', ease: 'none'});
+}
+
+const animationLeave = (container) => {
+    return gsap.to(container, { autoAlpha: 0, duration: 0.7, clearProps: 'all', ease: 'none'});
+}
+
+barba.init({
+    transitions: [
+        {
+            once({next}){
+                animationEnter(next.container);
+            },
+            leave: ({current}) => animationLeave(current.container),
+            enter({next}){
+                animationEnter(next.container);
+            }
+        }
+    ]
+})
