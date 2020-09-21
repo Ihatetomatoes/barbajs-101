6936f2f4039151a016abc893c144d74ccf5f0aad
Petr Tichy <peter@ihatetomatoes.net>
2020-09-21 18:32:17 +1000

    updated animationLeave

--- barba-gsap/src/js/animations/animationLeave.js
+++ barba-gsap/src/js/animations/animationLeave.js
@@ -1,7 +1,23 @@
 import gsap from 'gsap';
 
 const animationLeave = (container) => {
-    return gsap.to(container, { autoAlpha: 0, duration: 0.7, clearProps: 'all', ease: 'none'});
+
+    const activeLink = container.querySelector('a.is-active span');
+    const images = container.querySelectorAll('.image');
+    const img = container.querySelectorAll('img');
+
+    const tl = gsap.timeline({
+        defaults: {
+            duration: 0.4, ease: 'power1.in'
+        }
+    });
+
+    tl
+        .to(activeLink, { xPercent: 101}, 0)
+        .to(images, { xPercent: 101, stagger: 0.05 }, 0)
+        .to(img, { xPercent: -101, stagger: 0.05 }, 0);
+    
+    return tl;
 }
 
 export default animationLeave;
